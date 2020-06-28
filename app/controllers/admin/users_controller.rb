c5e8db8e7293fca9a5656405453d1a0cdbd8a7c6
Sandra <sandra.moutous@gmail.com>
2020-06-28 15:45:25 +0200

    Update front home + page about created

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,29 @@
 module Admin
-  class Userscontroller < AdminController
+  class UsersController < AdminController
+    before_action :set_user
+
+    def show; end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to admin_user_path, notice: t('users.updated')
+      else
+        redirect_to edit_admin_user_path, alert: t('users.not_updated')
+      end
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:firstname, :lastname, :description, :image)
+    end
   end
 end
